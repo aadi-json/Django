cc5b5885cbba51a77859bf3bd64863df22f2dcdf
aadi <adityagawande1122@gmail.com>
2026-05-05 23:12:46 +0530

    filters

--- ch10/course/views.py
+++ ch10/course/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# def learn_django(request):
+#     name="django"
+#     duration="30days"
+#     seats=30
+#     course_details={"name":name,"duration":duration, "seats":seats}
+#     return render(request,'course/django.html',course_details)
+
 def learn_django(request):
-    name="django"
-    duration="30days"
-    seats=30
-    course_details={"name":name,"duration":duration, "seats":seats}
-    return render(request,'course/django.html',course_details)
+    return render(request,'course/django.html', {"Name":"Django","Description":"Django is a awsome web framework "})
