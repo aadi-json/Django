c7320dbefd937befff85909e92905b216e6fb769
aadi <adityagawande1122@gmail.com>
2026-05-06 15:27:43 +0530

    filter done

--- ch10/course/views.py
+++ ch10/course/views.py
@@ -1,6 +1,10 @@
+from  datetime import datetime
 from django.shortcuts import render
-
+import time
 # Create your views here.
+
+
+#filters Example-1
 
 # def learn_django(request):
 #     name="django"
@@ -9,5 +13,19 @@ from django.shortcuts import render
 #     course_details={"name":name,"duration":duration, "seats":seats}
 #     return render(request,'course/django.html',course_details)
 
+#filters Example-2
+
+# def learn_django(request):
+#     return render(request,'course/django.html', {"Name":"Django","Description":"Django is a awsome web framework "})
+
+#filters Example-3
+
+# def learn_django(request):
+#     date=datetime.now()
+#     return render(request,'course/django.html', {"date":date,})
+
+#filters Example-4
+
 def learn_django(request):
-    return render(request,'course/django.html', {"Name":"Django","Description":"Django is a awsome web framework "})
+    date=datetime.now()
+    return render(request,'course/django.html', {"f1": 50.1234, "f2":55.4500, "f3":77.0000})
